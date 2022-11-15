76513c56affc0ad5526224e6497888863a3f2b96
Maël Quéau <mael.queau99@gmail.com>
2022-11-15 12:51:55 +0100

    chore: pre-commit lint setup and api module setup

---
+++ api/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
+
+const appEnv = dotenv.config();
+dotenvExpand.expand(appEnv);
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.APP_PORT, () => {
+  console.log("Example app listening on port 3000!");
+});
