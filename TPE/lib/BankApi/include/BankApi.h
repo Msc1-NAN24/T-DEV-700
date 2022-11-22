6f9dfb5a6ce676cc43bffdf8c2de40c31f003c61
Clément Boesmier <cle.chec@laposte.net>
2022-11-22 18:37:30 +0100

    add Bank Api lib

---
+++ TPE/lib/BankApi/include/BankApi.h
@@ -0,0 +1,50 @@
+#ifndef BANK_API_H
+#define BANK_API_H
+
+#include <Arduino.h>
+#include <ESP8266HTTPClient.h>
+//TODO: chager pour l'esp32
+
+#include <WiFiClient.h>
+
+class BankApi {
+public:
+
+
+    /**
+     * @brief Construct a new HTTPRequester object
+     *
+     * @param ip adress of the bank server
+     * @param port port of the bank server
+     */
+    BankApi(String ip, int port, String user, String password);
+
+    /**
+     * @brief receive value from the consumer account to the connected account
+     *
+     * @param userID id of the consumer
+     * @param amount amount to send
+     * @return true if the transaction is a success
+     */
+    bool receiveFrom(String userID, int amount);
+
+    bool login();//TODO: passer en privé a l'automatisation
+private:
+
+
+    String username;
+
+    String password;
+
+    String token;
+
+    String ip;
+    int port;
+
+    HTTPClient* http;
+    WiFiClient* client;
+
+};
+
+
+#endif
---
+++ TPE/lib/BankApi/src/BankApi.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include "../include/BankApi.h"
+#include <Arduino_JSON.h>
+
+
+BankApi::BankApi(String ip, int port, String user, String password) {
+    this->ip = ip;
+    this->port = port;
+    this->username = user;
+    this->password = password;
+
+    this->client = new WiFiClient();
+    this->http = new HTTPClient();
+    this->token = "";
+}
+
+bool BankApi::receiveFrom(String userID, int amount) {
+    bool sortie = true;
+    this->http->begin(*this->client, this->ip, this->port, "/api/users/" + userID + URL_PAYMENT);
+    this->http->addHeader("Authorization", "Bearer " + this->token);
+    this->http->addHeader("Content-Type", "application/json");
+    int resp = this->http->POST("{\"amount\": " + String(amount) + "}");
+    if (resp != 201) {
+        Serial.println("Error while sending money");
+        Serial.println(resp);
+        Serial.println(this->http->errorToString(resp));
+        Serial.println(this->http->getString());
+        sortie = false;
+    }
+    this->http->end();
+    return sortie;
+}
+
+bool BankApi::login() {
+    bool sortie = true;
+    this->http->begin(*this->client, this->ip, this->port, "/api/login");
+    this->http->addHeader("Content-Type", "application/json");
+    String body = "{\"username\": \"" + this->username + "\", \"password\": \"" + this->password + "\"}";
+    this->http->addHeader("Content-Length", String(body.length()));
+    int resp = this->http->POST(body);
+    if (resp != 200) {
+        Serial.println("connection error");
+        Serial.println(resp);
+        Serial.println(this->http->errorToString(resp));
+        Serial.println(this->http->getString());
+        sortie = false;
+    } else {
+        JSONVar json = JSON.parse(this->http->getString());
+        String temp = json.stringify(json["data"]["token"]);
+        this->token = temp.substring(1, temp.length() - 1);
+        Serial.println(this->token);
+    }
+    this->http->end();
+    return sortie;
+}
+
+
+
+    //this->http->begin(*this->client, this->ip, this->port, this->url);
