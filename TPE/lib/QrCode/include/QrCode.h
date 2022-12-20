3c75c3724622df8f934de75ccc1fffe5fd2db8f2
Clément Boesmier <cle.chec@laposte.net>
2022-12-20 15:56:37 +0100

    add doc to qrcode lib

--- TPE/lib/QrCode/include/QrCode.h
+++ TPE/lib/QrCode/include/QrCode.h
@@ -5,15 +5,37 @@
 
 class QrCode {
 public:
+
+    /**
+     *@brief Construct a new Qr Code object
+     *
+     */
     QrCode();
 
+    /**
+     * @brief envoie une demande de lecture au lecteur tout les 10s environ
+     *
+     * @return String renvoie la donnée brute lue par le capteur
+     */
     String read();
 
+
+    /**
+     *@brief Get the Uuid object
+     *
+     * @return String Uuid of the cheque
+     */
     String getUuid();
 
+    /**
+     *@brief Get the Amount object
+     *
+     * @return int Amount in the cheque
+     */
     int getAmount();
 
 private:
+
     /**
      * @brief resultat brut de la lecture du qrcode
      *
