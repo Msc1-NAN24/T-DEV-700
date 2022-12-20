a509cd120a629253e4a51c455835329a419a9bdd
Clément Boesmier <cle.chec@laposte.net>
2022-12-20 15:52:34 +0100

    add doc to bank api

--- TPE/lib/BankApi/include/BankApi.h
+++ TPE/lib/BankApi/include/BankApi.h
@@ -27,11 +27,23 @@ public:
      */
     bool receiveFrom(String userID, int amount);
 
-
+    /**
+     * @brief receive value from the card of the consumer account to the connected account
+     *
+     * @param NFCID id of nfc card
+     * @param amount amount to send
+     * @return true if the transaction is a success
+     */
     bool receiveFromNFC(String NFCID, int amount);
 
 private:
 
+    /**
+     * @brief get the jwt token to the server if the user is not connected
+     *
+     * @return true
+     * @return false
+     */
     bool login();
 
     String username;
