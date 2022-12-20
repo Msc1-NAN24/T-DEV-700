37c8882d0f82192af9af614d980bda2311998ee3
Clément Boesmier <cle.chec@laposte.net>
2022-12-20 15:53:06 +0100

    rm debuggin for prod

--- TPE/lib/BankApi/src/BankApi.cpp
+++ TPE/lib/BankApi/src/BankApi.cpp
@@ -14,7 +14,7 @@ BankApi::BankApi(String ip, int port, String user, String password) {
     this->token = "";
 }
 
-bool BankApi::receiveFrom(String userID, int amount) {//TODO: mettre l'animation de process dans la fonction d'api
+bool BankApi::receiveFrom(String userID, int amount) {
     bool sortie = true;
     this->http->begin(*this->client, this->ip, this->port, URL_CHEQUE + userID + URL_PAYMENT);
     this->http->addHeader("Authorization", "Bearer " + this->token);
@@ -24,10 +24,12 @@ bool BankApi::receiveFrom(String userID, int amount) {//TODO: mettre l'animation
         this->login();
         this->receiveFrom(userID, amount);
     } else if (resp != 201) {
+#ifdef DEBUG
         Serial.print("Error while sending money (QR) : ");
         Serial.println(resp);
         Serial.println(this->http->errorToString(resp));
         Serial.println(this->http->getString());
+#endif
         sortie = false;
     }
     this->http->end();
@@ -44,10 +46,12 @@ bool BankApi::receiveFromNFC(String NFCID, int amount) {
         this->login();
         this->receiveFromNFC(NFCID, amount);
     } else if (resp != 201) {
+#ifdef DEBUG
         Serial.println("Error while sending money (NFC) : ");
         Serial.println(resp);
         Serial.println(this->http->errorToString(resp));
         Serial.println(this->http->getString());
+#endif
         sortie = false;
     }
     this->http->end();
@@ -62,20 +66,19 @@ bool BankApi::login() {
     this->http->addHeader("Content-Length", String(body.length()));
     int resp = this->http->POST(body);
     if (resp != 200) {
+#ifdef DEBUG
         Serial.println("connection error");
         Serial.println(resp);
         Serial.println(this->http->errorToString(resp));
         Serial.println(this->http->getString());
+#endif
         sortie = false;
     } else {
         JSONVar json = JSON.parse(this->http->getString());
         String temp = json.stringify(json["data"]["token"]);
-        Serial.print(temp);
         this->token = temp.substring(1, temp.length() - 1);
-        Serial.println(this->token);
     }
     this->http->end();
     return sortie;
 }
 
-    //this->http->begin(*this->client, this->ip, this->port, this->url);
