1df3e114aeac7e621cafcac57df220917721adf9
Clément Boesmier <cle.chec@laposte.net>
2022-12-20 15:21:55 +0100

    cleanup programme principal

--- TPE/src/Program.cpp
+++ TPE/src/Program.cpp
@@ -28,29 +28,13 @@ Program::Program() {
     //init WiFi
     WiFi.mode(WIFI_STA);
     WiFiManager wm;
-    //wm.setDebugOutput(false); //FIXME : rm commentaire pour prod
+#ifndef DEBUG
+    wm.setDebugOutput(false);
+#endif
     wm.autoConnect(WIFI_DEFAULT_SSID, WIFI_DEFAULT_PASSWORD);
 }
 
 void Program::loop() {
-/*
-    String tag = this->NFC->read();
-    if (tag != "") {
-        Serial.println(tag);
-        delay(1000);
-    }
-    if (result == "") {
-        result = qrCode->read();
-    } else if (!temp) {
-        temp = true;
-        Serial.println(result);
-        Serial.println(qrCode->getAmount());
-        Serial.println(qrCode->getUuid());
-    }
-*/
-
-    // debut code principal
-
     if (Serial.available() > 0) {
         String sortie = "";
         while (Serial.available() > 0) {
@@ -90,7 +74,6 @@ void Program::loop() {
                 this->screen->process();
                 if (!transaction) {
                     this->screen->errorAnimation(" Transac: \n   Bank   ");
-                    //TODO: mettre ecran d'erreur requete incorrect
                 } else {
                     this->screen->validateAnimation();
                 }
@@ -102,7 +85,6 @@ void Program::loop() {
             this->screen->process();
             if (!transaction) {
                 this->screen->errorAnimation(" Transac: \n   Bank   ");
-                //TODO: mettre ecran d'erreur requete incorrect
             } else {
                 this->screen->validateAnimation();
             }
@@ -111,25 +93,4 @@ void Program::loop() {
     } else {
         this->screen->welcome();
     }
-
-    /*
-    algo :
-    j'attend un message serie
-    je le transforme en int
-    je l'affiche a l'écran le montant de la transaction
-
-    je lis le lecteur qrcode et le lecteur nfc
-    j'affiche l'ecran de chargement
-    si qrcode je compart le montant avec le montant envoyer
-        si diff je renvoie une erreur a l'app, j'affiche une erreur sur l'écran et je quite le process
-        si non j'envoie la requet au serveur de banque
-    si non si nfc j'envoie la requet au serveur avec l'id du nfc
-
-    j'attent une reponse du serveur
-    si requete valide j'affiche l'écran validée et je send l'id de transaction a l'app
-    si elle est invalide j'affiche un message d'érreur et send erreur a l'app
-    */
 }
-
-
-//TODO: faire custom field on config (voir :https://www.youtube.com/watch?v=VnfX9YJbaU8)
